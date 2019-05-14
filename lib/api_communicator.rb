fd77dea6fcaafa94508269248b5fecbfc5249fed
Chet  Chopra <chopra.chet@gmail.com>
2019-05-14 23:08:38 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -13,6 +13,8 @@ def get_character_movies_from_api(character_name)
   results.each do |result|
     if result["name"] == character_name
       films = result["films"]
+    end
+  end
       
 
   # iterate over the response hash to find the collection of `films` for the given
