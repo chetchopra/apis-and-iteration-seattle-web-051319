6e83d8db2968a11cc2183f62de8637d5775e2f0f
Chet  Chopra <chopra.chet@gmail.com>
2019-05-14 23:25:41 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -24,17 +24,6 @@ def get_character_movies_from_api(character_name)
   end
   
   return film_titles
-      
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
 def print_movies(films)
