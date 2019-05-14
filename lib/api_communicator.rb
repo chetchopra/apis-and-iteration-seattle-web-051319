a1b9f1cb0ca14dac18d8dedf4a5e972d8559ae11
Chet  Chopra <chopra.chet@gmail.com>
2019-05-14 23:06:31 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,7 +7,9 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
   
-  films = response_hash["films"]
+  films = response_hash["results"]
+  
+  films.each do ||
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
